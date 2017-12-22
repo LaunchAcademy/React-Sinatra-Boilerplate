a2e500b53c87825eba983fda1878a38d5a74fde7
Nick Alberts <nwalberts5@gmail.com>
2017-12-22 16:01:36 -0500

    init commit

---
+++ src/containers/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Frame from '../components/Frame.js'
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    )
+  }
+}
+
+export default App
